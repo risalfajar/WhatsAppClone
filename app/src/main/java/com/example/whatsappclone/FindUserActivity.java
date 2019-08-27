197cd7be65f5096ca3a910622644849832e76c9a
risalfajar <risalfajar@gmail.com>
2019-08-27 14:24:45 +0700

    Add contacts and find users feature

--- app/src/main/java/com/example/whatsappclone/FindUserActivity.java
+++ app/src/main/java/com/example/whatsappclone/FindUserActivity.java
@@ -2,20 +2,30 @@ package com.example.whatsappclone;
 
 import android.database.Cursor;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class FindUserActivity extends AppCompatActivity {
-
+    private static final String TAG = "FindUserActivity";
     private RecyclerView mUserList;
     private RecyclerView.Adapter mUserListAdapter;
     private RecyclerView.LayoutManager mUserListLayoutManager;
 
-    ArrayList<UserObject> userList;
+    ArrayList<UserObject> userList, contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +33,15 @@ public class FindUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_find_user);
 
         userList = new ArrayList<>();
+        contactList = new ArrayList<>();
 
         initializeRecyclerView();
         getContactList();
     }
 
     private void getContactList(){
-        //get all contacts
+        String IsoPrefix = getCountryISO();
+        //get all contacts in the phone
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 null, null, null, null);
 
@@ -37,12 +49,70 @@ public class FindUserActivity extends AppCompatActivity {
             String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String phone= phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
+            //normalizing phone string
+            //remove characters other than 0-9 and +
+            phone = phone.replaceAll("[^0-9+]", "");
+            Log.d(TAG, "Phone number after regex:" + phone);
+
+            //if the phone doesn't have iso prefix
+            if(!String.valueOf(phone.charAt(0)).equals("+"))
+                phone = IsoPrefix + phone;
+
+            Log.d(TAG, "Phone number after check ISO:" + phone);
+
             UserObject mContact = new UserObject(name, phone);
-            userList.add(mContact);
+            contactList.add(mContact);
+            getUserDetails(mContact);
         }
         //notify adapter that contact has been loaded
         mUserListAdapter.notifyDataSetChanged();
         phones.close();
+    }
+
+    /* if the contact in the @mContact are exist in the firebase database
+    ** add it to contact list
+     */
+    private void getUserDetails(final UserObject mContact) {
+        //get data from database
+        DatabaseReference mUserDB = FirebaseDatabase.getInstance().getReference().child("user");
+        //fetch only one user that equals to parameter
+        Query query = mUserDB.orderByChild("phone").equalTo(mContact.getPhone());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    Log.d(TAG, "Data " + mContact.getPhone() + " exists!");
+                    String phone = "",
+                            name = mContact.getName();
+                    for(DataSnapshot childSnapshot : dataSnapshot.getChildren()){
+                        if(childSnapshot.child("phone").getValue() != null)
+                            phone = childSnapshot.child("phone").getValue().toString();
+
+                        UserObject mUser = new UserObject(name, phone);
+                        userList.add(mUser);
+                        mUserListAdapter.notifyDataSetChanged();
+                        return;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private String getCountryISO(){
+        String iso = null;
+        //contain information of our phone ISO
+        TelephonyManager telephonyManager = (TelephonyManager)getApplicationContext().getSystemService(getApplicationContext().TELEPHONY_SERVICE);
+        if (telephonyManager.getNetworkCountryIso() != null) {
+            if(!telephonyManager.getNetworkCountryIso().equals(""))
+                iso = telephonyManager.getNetworkCountryIso();
+        }
+
+        return CountryToPhonePrefix.getPhone(iso);
     }
 
     private void initializeRecyclerView() {
--- app/src/main/java/com/example/whatsappclone/LoginActivity.java
+++ app/src/main/java/com/example/whatsappclone/LoginActivity.java
@@ -18,10 +18,19 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
 
     private EditText mPhoneNumber, mCode;
     private Button mSend;
@@ -46,13 +55,14 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(mVerificationId != null)
-                    verifyPhoneNumberWithCode();
+                    verifyPhoneNumberWithCode(); //this is called after phone doesn't verify automatically and the user have inputted verification code
                 else
-                    startPhoneNumberVerification();
+                    startPhoneNumberVerification(); //verify phone number inputted by the user
             }
         });
 
         mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            //this is called if the phone verify code automatically
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
                 signInWithPhoneAuthCredential(phoneAuthCredential);
@@ -63,8 +73,9 @@ public class LoginActivity extends AppCompatActivity {
 
             }
 
+            //this is called when a verification code is sent to the user (via sms)
             @Override
-            public void onCodeSent(String verificationId, PhoneAuthProvider.ForceResendingToken forceResendingToken) { //this is called whenever a code is sent to the user
+            public void onCodeSent(String verificationId, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
                 super.onCodeSent(verificationId, forceResendingToken);
 
                 mVerificationId = verificationId;
@@ -77,10 +88,42 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) { //task tells us if sign in is successful or not
-                if(task.isSuccessful())
-                    userIsLoggedIn();
-                else
-                    Toast.makeText(getApplicationContext(), "Task is not successfull", Toast.LENGTH_SHORT).show();
+            if(task.isSuccessful()){
+                //get all information available in the authentication page
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if(user != null){
+                    //use final because we will use it inside listener
+                    final DatabaseReference mUserDB = FirebaseDatabase.getInstance().getReference().child("user").child(user.getUid());
+                    //use singleValueEvent because we only get the data once
+                    mUserDB.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            //This method is called once with the initial value when activity starts
+                            //addListenerForSingleValueEvent() executes onDataChange method immediately and
+                            //after executing that method once, it stops listening to the reference location it is attached to
+
+                            //if the user hasn't been inputted to DB then we input it
+                            //basically it's register the user data for the first time user login
+                            if(!dataSnapshot.exists()){
+                                Map<String, Object> userMap = new HashMap<>();
+                                userMap.put("phone", user.getPhoneNumber());
+                                userMap.put("name", user.getPhoneNumber());
+                                mUserDB.updateChildren(userMap);
+                            }
+                            //login is done, move to next activity
+                            userIsLoggedIn();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+            else
+                Toast.makeText(getApplicationContext(), "Task is not successfull", Toast.LENGTH_SHORT).show();
             }
         });
     }
